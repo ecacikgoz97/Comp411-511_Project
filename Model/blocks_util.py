9206c75cbc7924d349a9bd1950148daa900ebbdb
ecacikgoz97 <ecacikgoz97@gmail.com>
2021-12-11 22:41:25 +0300

    shuffle block added

--- Model/blocks_util.py
+++ Model/blocks_util.py
@@ -21,3 +21,23 @@ def conv_block(in_channel, out_channel, kernel_size, stride=1, padding=0, groups
         nn.LeakyReLU(negative_slope=0.2, inplace=True),
     )
 
+def pixel_shuffle_block(in_channel, out_channel, upscale_factor=2, kernel_size=3, stride=1, padding=0, bias=True):
+    """
+        Pixel Shuffle Layer
+    """
+    return nn.Sequential(
+        conv_block(in_channel=in_channel, out_channel=out_channel * (upscale_factor ** 2), kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+        nn.PixelShuffle(upscale_factor=upscale_factor),
+        nn.BatchNorm2d(out_channel, affine=True),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+    )
+
+def calculate_parameters(model):
+    parameters = 0
+    for weight in model.parameters():
+        p = 1
+        for dim in weight.size():
+            p *= dim
+        parameters += p
+    return parameters
+
