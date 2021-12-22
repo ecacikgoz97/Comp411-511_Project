e36cc7044ef742e7df493199c1908dda8985fcaf
ecacikgoz97 <ecacikgoz97@gmail.com>
2021-12-23 01:26:07 +0300

    updated: stored as back-ups, not used in HGSR

--- Model/blocks_util.py
+++ Model/blocks_util.py
@@ -1,37 +1,6 @@
 import torch
 import torch.nn as nn
 
-class ConcatBlock(nn.Module):
-    """
-    Concatenate the output of a submodule to its input.
-    """
-    def __init__(self, sub_module):
-        super(ConcatBlock, self).__init__()
-        self.submodule = sub_module
-
-    def forward(self, x):
-        output = torch.cat((x, self.submodule(x)), dim=1)
-        return output
-
-
-def conv_block(in_channel, out_channel, kernel_size, stride=1, padding=0, groups=1, bias=True):
-    return nn.Sequential(
-        nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=bias),
-        nn.BatchNorm2d(out_channel, affine=True),
-        nn.LeakyReLU(negative_slope=0.2, inplace=True),
-    )
-
-def pixel_shuffle_block(in_channel, out_channel, upscale_factor=2, kernel_size=3, stride=1, padding=0, bias=True):
-    """
-        Pixel Shuffle Layer
-    """
-    return nn.Sequential(
-        conv_block(in_channel=in_channel, out_channel=out_channel * (upscale_factor ** 2), kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
-        nn.PixelShuffle(upscale_factor=upscale_factor),
-        nn.BatchNorm2d(out_channel, affine=True),
-        nn.LeakyReLU(negative_slope=0.2, inplace=True),
-    )
-
 def calculate_parameters(model):
     parameters = 0
     for weight in model.parameters():
@@ -41,3 +10,56 @@ def calculate_parameters(model):
         parameters += p
     return parameters
 
+class RIB(nn.Module):
+    """
+        Residual Inception BLock For SR without Norm Layer (for multi-scale processing)
+        conv 1*1  conv 1*1  conv 1*1
+                  conv 3*3  conv 3*3
+                            conv 3*3
+                  concat
+                  conv 1*1
+    """
+    def __init__(self, in_channel=64, out_channel=128, activation_inplace=True):
+        super(RIB, self).__init__()
+
+        self.conv1_1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.relu1_1 = nn.LeakyReLU(0.2, inplace=activation_inplace)
+
+        self.conv2_1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv2_2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu2_2 = nn.LeakyReLU(0.2, inplace=activation_inplace)
+
+        self.conv3_1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv3_2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu3_2 = nn.LeakyReLU(0.2, inplace=activation_inplace)
+        self.conv3_3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu3_3 = nn.LeakyReLU(0.2, inplace=activation_inplace)
+
+        self.filter_concat = nn.Conv2d(in_channels=3*out_channel, out_channels=out_channel, kernel_size=1, stride=1, padding=0, bias=False)
+
+        if in_channel != out_channel:
+            self.identity = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, padding=0, bias=False)
+        else:
+            def identity(tensor):
+                return tensor
+            self.identity = identity
+
+    def forward(self, x):
+        res1 = self.conv1_1(x)
+        res1 = self.relu1_1(res1)
+
+        res2 = self.conv2_1(x)
+        res2 = self.conv2_2(res2)
+        res2 = self.relu2_2(res2)
+
+        res3 = self.conv3_1(x)
+        res3 = self.conv3_2(res3)
+        res3 = self.relu3_2(res3)
+        res3 = self.conv3_3(res3)
+        res3 = self.relu3_3(res3)
+
+        res = torch.cat((res1, res2, res3), dim=1)
+        res = self.filter_concat(res)
+        x = self.identity(x)
+        out = torch.add(x, res)
+        return out
