46533d947273561fc0de41c673ff28c004d18f1d
ecacikgoz97 <ecacikgoz97@gmail.com>
2021-12-23 01:24:57 +0300

    HGSR implemented

--- Model/hgsr.py
+++ Model/hgsr.py
@@ -1,13 +1,12 @@
 import torch.nn as nn
-from blocks_model import RB, RIB, Encoder, Decoder
-from blocks_util import conv_block, pixel_shuffle_block
+from blocks_model import conv_block, RB, Encoder, Decoder, pixel_shuffle_block
 import math
 
 class HG_Block(nn.Module):
     """
     Hour Glass Block for SR Model
     """
-    def __init__(self):
+    def __init__(self, n_mid=2):
         super(HG_Block, self).__init__()
 
         self.down_sample1 = Encoder(in_channel=64, out_channel=128)
@@ -15,14 +14,10 @@ class HG_Block(nn.Module):
         self.down_sample3 = Encoder(in_channel=128, out_channel=256)
         self.down_sample4 = Encoder(in_channel=256, out_channel=256)
 
-        self.mid_res1 = RB(in_channel=256, out_channel=256) # Can be replace with RB
-        self.mid_res2 = RB(in_channel=256, out_channel=256)
-
-#        self.skip_conv0 = RIB(in_channel=64, out_channel=64)
-#        self.skip_conv1 = RIB(in_channel=128, out_channel=128)
-#        self.skip_conv2 = RIB(in_channel=128, out_channel=128)
-#        self.skip_conv3 = RIB(in_channel=256, out_channel=256)
-#        self.skip_conv4 = RIB(in_channel=256, out_channel=256)
+        res_block = []
+        for i in range(n_mid):
+            res_block.append(RB(256, 256))
+        self.mid_res = nn.Sequential(*res_block)
 
         self.up_sample1 = Decoder(in_channel=256, out_channel=256)
         self.up_sample2 = Decoder(in_channel=256, out_channel=128)
@@ -35,8 +30,7 @@ class HG_Block(nn.Module):
         x, res3 = self.down_sample3(x)
         x, res4 = self.down_sample4(x)
 
-        x = self.mid_res1(x)
-        x = self.mid_res2(x)
+        x = self.mid_res(x)
 
         x = self.up_sample1(x, res4)
         x = self.up_sample2(x, res3)
@@ -45,6 +39,9 @@ class HG_Block(nn.Module):
         return x
 
 class HGSR(nn.Module):
+    """
+    Hour Glass SR Model
+    """
     def __init__(self, in_channel=3, out_channel=3, upscale=4, nf=64, n_mid=2, n_HG=3):
         super(HGSR, self).__init__()
         self.n_HG = n_HG
@@ -52,10 +49,10 @@ class HGSR(nn.Module):
         if upscale == 3:
             n_upscale = 1
 
-        self.conv = conv_block(in_channel=in_channel, out_channel=nf, kernel_size=3)
-        LR_conv = conv_block(in_channel=nf, out_channel=nf, kernel_size=3)
-        HR_conv1 = conv_block(in_channel=nf, out_channel=nf, kernel_size=3)
-        HR_conv2 = conv_block(in_channel=nf, out_channel=out_channel, kernel_size=3)
+        self.conv = conv_block(in_channel=in_channel, out_channel=nf, kernel_size=3, padding=1)
+        LR_conv = conv_block(in_channel=nf, out_channel=nf, kernel_size=3, padding=1)
+        HR_conv1 = conv_block(in_channel=nf, out_channel=nf, kernel_size=3, padding=1)
+        HR_conv2 = conv_block(in_channel=nf, out_channel=out_channel, kernel_size=3, padding=1)
 
         if upscale == 3:
             upsampler = pixel_shuffle_block(in_channel=nf, out_channel=nf)
@@ -70,7 +67,7 @@ class HGSR(nn.Module):
         )
 
         for i in range(n_HG):
-            HG = HG_Block()
+            HG = HG_Block(n_mid=n_mid)
             setattr(self, 'HG_%d' % i, HG)
             setattr(self, 'upsample_%d' % i, upsample)
 
